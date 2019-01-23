c30356841687fc61b24dbaa945c7f1ebe684379d
boniqa <budnik.wer@gmail.com>
2019-01-23 12:08:00 +0100

    fixes

--- app/components/gitController.js
+++ app/components/gitController.js
@@ -8,8 +8,6 @@ $scope.popoverEmoji = {
   $scope.cb = function(emoji){
 	    insertText(emoji, "emojiInput");
   }
-
-  $scope.isClosed = true;
   
   var re = new RegExp(/\:[A-Za-z0-9\_\:]+/g);
   $scope.emojiList = emojiService.emojiList;
@@ -27,20 +25,18 @@ $scope.popoverEmoji = {
 			return true;
 		}
 	}
-	
-  }
+  };
 
-  
   $scope.stopEvent = function(event){
 	var keyPress = event.keyCode;
 	if($scope.toDisplay === true && (keyPress == 9 || keyPress == 39 || keyPress == 37 || keyPress == 27)){
 		event.preventDefault();
 		event.stopPropagation();
 	}
-  }
+  };
   
   function checkOffset(){
-	var res = false;
+	var correctPlasement = false;
 	if($scope.myElement){
 		var input = document.getElementById("emojiInput");
 		for(var elementIndex in $scope.myElement){
@@ -48,135 +44,116 @@ $scope.popoverEmoji = {
 			var offset = input.value.indexOf(element);
 			if($scope.focusOffset == offset + element.length){
 				$scope.elementIndex = elementIndex;
-				res = true;
+				correctPlasement = true;
 				break;
 			}
 		}
 	}
-	return res;
+	return correctPlasement;
   }
 
   $scope.searchForEmojis = function($event){
-	var keyPress = $event.keyCode;
-	if($scope.toDisplay === true && (keyPress == 9 || keyPress == 39 || keyPress == 37)){
-		return;
-	} else if (keyPress == 27){
-		return;
-	}
-	$scope.focusOffset = $event.target.selectionStart;
-	if($scope.searchingForShortcut !== undefined){
-		$scope.myElement = $scope.searchingForShortcut.match(re);
-		//console.log($scope.myElement[0]);
-		if(!checkOffset()){
-			$scope.myElement = [];
-			$scope.toDisplay = false;
-			$scope.currentIndex = 0;
+		var keyPress = $event.keyCode;
+		if($scope.toDisplay === true && (keyPress == 9 || keyPress == 39 || keyPress == 37)){
+			return;
+		} else if (keyPress == 27){
 			return;
 		}
-		$scope.emojisToDisplay = $scope.emojiList.filter(filterByShortname);
-
-		
-		
-		if ($scope.emojisToDisplay.length > 0){
-			$scope.toDisplay = true;
-
-			window.addEventListener("click", function() {
-				// $scope.closePicker();
+		$scope.focusOffset = $event.target.selectionStart;
+		if($scope.searchingForShortcut !== undefined){
+			//match list of elements from input with regex
+			$scope.myElement = $scope.searchingForShortcut.match(re);
+			
+			//checking if cursor in input is at the end of an element from myElement list
+			if(!checkOffset()){
 				$scope.myElement = [];
 				$scope.toDisplay = false;
 				$scope.currentIndex = 0;
-				$scope.$apply();
-				
-			});
+				return;
+			}
+			
+			$scope.emojisToDisplay = $scope.emojiList.filter(filterByShortname);
+			
+			if ($scope.emojisToDisplay.length > 0){
+				$scope.toDisplay = true;
+
+				window.addEventListener("click", function() {
+					$scope.myElement = [];
+					$scope.toDisplay = false;
+					$scope.currentIndex = 0;
+					$scope.$apply();
+					
+				});
+			}
+			else{
+					$scope.myElement = [];
+					$scope.toDisplay = false;
+					$scope.currentIndex = 0;
+			}
 		}
-		else{
-				$scope.myElement = [];
-				$scope.toDisplay = false;
-				$scope.currentIndex = 0;
-		}
-		// console.log($scope.emojisToDisplay);	
-		
-	}
-	
   };
 
-$scope.closePicker = function(){
-	$scope.myElement = [];
-	$scope.currentIndex = 0;
-	$scope.toDisplay = false;
-	$scope.$applyAsync();
-}
   $scope.currentIndex = 0;
 
-  
   $scope.keyDown = function(keyPress){
 	  if($scope.toDisplay === true){
 		  //enter and right-arrow
-		if(keyPress == 9 || keyPress == 39){
-			if($scope.currentIndex == $scope.emojisToDisplay.length -1 || $scope.currentIndex == 39){
-				$scope.currentIndex = 0;
+			if(keyPress == 9 || keyPress == 39){
+				if($scope.currentIndex == $scope.emojisToDisplay.length -1 || $scope.currentIndex == 19){
+					$scope.currentIndex = 0;
+					$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
+				}
+				else{
+					$scope.currentIndex++;
+					$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
+				}
+				
+			}
+			//left arrow
+			else if(keyPress == 37){
+				if($scope.currentIndex == 0){
+					$scope.currentIndex = $scope.emojisToDisplay.length > 20 ? 19 : $scope.emojisToDisplay.length -1;
+					$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
+				}
+				else{
+					$scope.currentIndex--;
+					$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
+				}
+				
+			}
+			//enter
+			else if(keyPress == 13){
 				$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
+				$scope.myElement = [];
+				$scope.toDisplay = false;			
 			}
-			else{
-			  $scope.currentIndex++;
-			  $scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
+			//escape
+			else if(keyPress == 27){		  
+				$scope.myElement = [];
+				$scope.toDisplay = false; 
 			}
-			
-		}
-		//left arrow
-		else if(keyPress == 37){
-		  if($scope.currentIndex == 0){
-			  $scope.currentIndex = $scope.emojisToDisplay.length > 40 ? 39 : $scope.emojisToDisplay.length -1;
-			  $scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
-		  }
-		  else{
-			  $scope.currentIndex--;
-			  $scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
-		  }
-			
-		}
-		//enter
-		else if(keyPress == 13){
-			$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
-		  
-		  $scope.myElement = [];
-		  $scope.toDisplay = false;
-		 
-			
-		}
-		//escape
-		else if(keyPress == 27){		  
-		  $scope.myElement = [];
-		  $scope.toDisplay = false;
-		  
-			
-		}
 	  }
-	  
 	  
   }
   
   $scope.pickEmoji = function(code){
-	var emoji = $scope.convert(code) + ' ';
-	
-	// insertText(emoji, "emojiInput");
-	var input = document.getElementById("emojiInput");
-	var regex = new RegExp($scope.myElement[$scope.elementIndex], 'g');
+		var emoji = $scope.convert(code) + ' ';
+		var input = document.getElementById("emojiInput");
+		var regex = new RegExp($scope.myElement[$scope.elementIndex], 'g');
 
-	var newVal = input.value.replace(regex, function(match, offset, string){
-		
-		if(offset + match.length === $scope.focusOffset){
-			return emoji;
-		}
-		return match;
-		});
+		var newVal = input.value.replace(regex, function(match, offset, string){
+			
+			if(offset + match.length === $scope.focusOffset){
+				return emoji;
+			}
+			return match;
+			});
 
-	$scope.focusOffset = $scope.focusOffset - ($scope.myElement[$scope.elementIndex].length - emoji.length);
-	$scope.myElement[$scope.elementIndex] = emoji;
-	
-	input.value = newVal;
-	input.selectionStart = $scope.focusOffset;
-	input.selectionEnd = $scope.focusOffset;
+		$scope.focusOffset = $scope.focusOffset - ($scope.myElement[$scope.elementIndex].length - emoji.length);
+		$scope.myElement[$scope.elementIndex] = emoji;
+		input.value = newVal;
+		input.selectionStart = $scope.focusOffset;
+		input.selectionEnd = $scope.focusOffset;
 	
 	};
 
@@ -190,43 +167,41 @@ $scope.closePicker = function(){
 		$scope.pickEmoji($scope.emojisToDisplay[$scope.currentIndex].output);
 	}	
 	  
-
   function insertText(text, id) {
+		var input = document.getElementById(id);
+		if (input === undefined) { return; }
 
-	var input = document.getElementById(id);
-	if (input === undefined) { return; }
-
-	var scrollPos = input.scrollTop;
-	var pos = 0;
-	var browser = ((input.selectionStart || input.selectionStart == "0") ?
-				   "ff" : (document.selection ? "ie" : false));
-	if (browser == "ie") {
-		input.focus();
-		var range = document.selection.createRange();
-		range.moveStart("character", -input.value.length);
-		pos = range.text.length;
+		var scrollPos = input.scrollTop;
+		var pos = 0;
+		var browser = ((input.selectionStart || input.selectionStart == "0") ?
+						"ff" : (document.selection ? "ie" : false));
+		if (browser == "ie") {
+			input.focus();
+			var range = document.selection.createRange();
+			range.moveStart("character", -input.value.length);
+			pos = range.text.length;
+		}
+		else if (browser == "ff") {
+			pos = input.selectionStart;
+		}
+		var front = (input.value).substring(0, pos);
+		var back = (input.value).substring(pos, input.value.length);
+		input.value = front + text + back;
+		pos = pos + text.length;
+		if (browser == "ie") {
+			input.focus();
+			var range = document.selection.createRange();
+			range.moveStart("character", -input.value.length);
+			range.moveStart("character", pos);
+			range.moveEnd("character", 0);
+			range.select();
+		}
+		else if (browser == "ff") {
+			input.selectionStart = pos;
+			input.selectionEnd = pos;
+			input.focus();
+		}
+		input.scrollTop = scrollPos;
+		angular.element(input).trigger('input');
 	}
-	else if (browser == "ff") {
-		pos = input.selectionStart;
-	}
-	var front = (input.value).substring(0, pos);
-	var back = (input.value).substring(pos, input.value.length);
-	input.value = front + text + back;
-	pos = pos + text.length;
-	if (browser == "ie") {
-		input.focus();
-		var range = document.selection.createRange();
-		range.moveStart("character", -input.value.length);
-		range.moveStart("character", pos);
-		range.moveEnd("character", 0);
-		range.select();
-	}
-	else if (browser == "ff") {
-		input.selectionStart = pos;
-		input.selectionEnd = pos;
-		input.focus();
-	}
-	input.scrollTop = scrollPos;
-	angular.element(input).trigger('input');
-}
 }]);
